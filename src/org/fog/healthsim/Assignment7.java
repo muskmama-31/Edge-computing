8c5e2525b6e16749fbb64925e5fce31271dfcdb4
muskmama-31 <saisabarishguntupalli@gmail.com>
2026-01-02 20:35:52 +0530

    Added all

---
+++ src/org/fog/healthsim/Assignment7.java
@@ -0,0 +1,15 @@
+1. What is a Raspberry Pi, What is its primary purpose, and What kind of computer is it? 
+
+2. Identify and label different components of Raspberry Pi Hardware. 
+
+Take a photo and annotate all components.
+CPU
+GPIO Pins
+USB Ports
+HDMI Ports
+Power Port
+Camera Interface
+Ethernet Port (if available)
+3. What is the main Operating system for Raspberry Pi?
+
+4.  How Raspberry Pi can be used for edge computing and what are its benefits?
